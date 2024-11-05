97badf606447c94edda2065bdd510b5d730ce204
alexhamill <60296840+alexhamill@users.noreply.github.com>
2024-11-05 16:15:58 -0500

    adding more js

--- websites/firstpage/firstjs.js
+++ websites/firstpage/firstjs.js
@@ -1,2 +1,10 @@
 function changecolor(event){
   event.currentTarget.classList.toggle('active');}
+
+function listiners(){
+  var button = document.querySelectorAll('.box');
+  button.forEach(function(button) {
+    button.addEventListener('click', changecolor)
+  });
+
+}
