9f98aa6a342db0b1aa7c4d13771ecd46a2794924
alexhamill <60296840+alexhamill@users.noreply.github.com>
2024-11-07 13:02:53 -0500

    work plz added the random stuff

---
+++ websites/animation/animation.js
@@ -0,0 +1,18 @@
+function move(event) {
+    var cube = event.currentTarget;
+    const ranx = Math.floor(Math.random()*(window.innerWidth-50));
+    const rany = Math.floor(Math.random()*(window.innerHeight-50));
+    cube.style.transform = 'translate(${ranx}px, ${rany}px'
+
+  }
+
+  
+  
+  function Listeners() {
+    var cubes = document.querySelectorAll('.cube'); 
+    cubes.forEach(function(cube) {
+      box.addEventListener('mouseenter', move);
+    });
+  }
+  
+  Listeners();
