a6e092efac5bddf48de4fd65eac6fa04abab47ba
alexhamill <60296840+alexhamill@users.noreply.github.com>
2024-11-08 00:09:10 -0500

    testignting

--- websites/animation/animation.js
+++ websites/animation/animation.js
@@ -3,7 +3,8 @@ function move(event) {
     var cube = event.currentTarget;
     const ranx = Math.floor(Math.random()*(window.innerWidth-50));
     const rany = Math.floor(Math.random()*(window.innerHeight-50));
-    cube.style.transform = `translate(${ranx}px, ${rany}px`
+    cube.offsetLeft = ranx;
+    cube.offsetTop = rany;
     cube.classList.add('active');
     score = score + 1;
     document.querySelector('.score').textContent = score;
