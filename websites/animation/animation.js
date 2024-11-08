2009f5de5c0b8047939757abe8e7f8bdfd06979c
alexhamill <60296840+alexhamill@users.noreply.github.com>
2024-11-08 08:00:26 -0500

    removeing yet another semi colon

--- websites/animation/animation.js
+++ websites/animation/animation.js
@@ -23,7 +23,7 @@ function move(event) {
     const c1 = document.querySelector(".cube1");
     const c2 = document.querySelector(".cube2");
     const c3 = document.querySelector(".cube3");
-    var code = score + "$" + c1.offsetLeft + "$" + c1.offsetTop + "$" + c2.offsetLeft + "$" + c2.offsetTop + c3.offsetLeft; + "$" + c3.offsetTop + "$";
+    var code = score + "$" + c1.offsetLeft + "$" + c1.offsetTop + "$" + c2.offsetLeft + "$" + c2.offsetTop + c3.offsetLeft + "$" + c3.offsetTop + "x";
     return code;
   }
   function getcode(){
