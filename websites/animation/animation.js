0be2245c691c60462231256ed2c652c025b51001
alexhamill <60296840+alexhamill@users.noreply.github.com>
2024-11-07 23:10:13 -0500

    adding score

--- websites/animation/animation.js
+++ websites/animation/animation.js
@@ -1,10 +1,12 @@
+var score = 0;  
 function move(event) {
     var cube = event.currentTarget;
     const ranx = Math.floor(Math.random()*(window.innerWidth-50));
     const rany = Math.floor(Math.random()*(window.innerHeight-50));
     cube.style.transform = `translate(${ranx}px, ${rany}px`
     cube.classList.add('active');
-
+    score = score + 1;
+    document.querySelector('.score').textContent = score;
   }
 
   
