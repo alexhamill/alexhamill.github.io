245ec530ccbcdaf0e969800caad951c4db54fbd4
alexhamill <60296840+alexhamill@users.noreply.github.com>
2024-11-08 13:18:44 -0500

    tetsing

--- websites/animation/animation.js
+++ websites/animation/animation.js
@@ -30,6 +30,8 @@ function move(event) {
     document.querySelector(".code").textContent=exp();
   }
   function decode(){
+    var str = "a$b$c$d$f"
+    console.log(bindexOf(str, "$", 3));
     var code = document.getElementById("code").value;
     console.log(code);
     const c1 = document.querySelector(".cube1");
@@ -62,9 +64,12 @@ function move(event) {
     cube.classList.toggle('active');
   }
   function bindexOf(str,s,n){
-    var curentpos = 0;
+    var curentpos = -1;
     for (let i=0; i<n-1;i++){
       curentpos = str.indexOf(s,curentpos+1);
+      if (curentpos === -1){
+        break;
+      }
     }
     return curentpos;
   }
