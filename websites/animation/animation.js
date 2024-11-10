ecd9b3c7c6df74a72ffe6b060ca66a8757ea49cf
alexhamill <60296840+alexhamill@users.noreply.github.com>
2024-11-10 00:58:56 -0500

    clicker

--- websites/animation/animation.js
+++ websites/animation/animation.js
@@ -1,19 +1,21 @@
- 
+let clicker = 0; 
+let score = 0;
 function move(event) {
-    var cube = event.currentTarget;
+    let cube = event.currentTarget;
     const ranx = Math.floor(Math.random()*(window.innerWidth-50));
     const rany = Math.floor(Math.random()*(window.innerHeight-50));
     cube.style.left = ranx + 'px';
     cube.style.top = rany + 'px';
     cube.classList.add('active');
-    document.querySelector('.score').textContent = score = Number(document.querySelector('.score').textContent) + 1;
+    score += 1;
+    document.querySelector('.score').textContent = score;
     getcode();
   }
 
   
   
   function Listeners() {
-    var cubes = document.querySelectorAll('.cube'); 
+    let cubes = document.querySelectorAll('.cube'); 
     cubes.forEach(function(cube) {
       cube.addEventListener('mouseenter', move);
     });
@@ -23,7 +25,7 @@ function move(event) {
     const c1 = document.querySelector(".cube1");
     const c2 = document.querySelector(".cube2");
     const c3 = document.querySelector(".cube3");
-    var code = "s" + document.querySelector('.score').textContent + "$" + c1.style.left + "$" + c1.style.top + "$" + c2.style.left + "$" + c2.style.top +"$"+ c3.style.left + "$" + c3.style.top + "x";
+    let code = "s" + score + "$" + c1.style.left + "$" + c1.style.top + "$" + c2.style.left + "$" + c2.style.top +"$"+ c3.style.left + "$" + c3.style.top + "x";
     code = code.replaceAll('px',"");
     return code;
   }
@@ -31,9 +33,7 @@ function move(event) {
     document.getElementById("displaycode").textContent=exp();
   }
   function decode(){
-    var str = "a$b$c$d$f"
-    console.log(bindexOf(str, "$", 3));
-    var code = document.getElementById("code").value;
+    let code = document.getElementById("code").value;
     console.log(code);
     const c1 = document.querySelector(".cube1");
     const c2 = document.querySelector(".cube2");
@@ -41,7 +41,7 @@ function move(event) {
     c1.classList.add('active');
     c2.classList.add('active');
     c3.classList.add('active');
-    var score = Number(code.substring(code.indexOf("s")+1,code.indexOf("$")));
+    score = Number(code.substring(code.indexOf("s")+1,code.indexOf("$")));
     document.querySelector('.score').textContent = score;
     
     c1.style.left = Number(code.substring(bindexOf(code,"$",1)+1,bindexOf(code,"$",2))) + 'px';
@@ -63,12 +63,15 @@ function move(event) {
     
   }
 
-  function opensetings(event) {
-    var cube = document.querySelector(".settings");
+  function opensetings() {
+    let cube = document.querySelector(".settings");
     cube.classList.toggle('active');
   }
+  function openshop(){
+    document.getElementById("storecontainer").classList.toggle("active");
+  }
   function bindexOf(str,s,n){
-    var curentpos = -1;
+    let curentpos = -1;
     for (let i=0; i<n;i++){
       curentpos = str.indexOf(s,curentpos+1);
       if (curentpos === -1){
@@ -79,12 +82,20 @@ function move(event) {
   }
   
   function Listenerss() {
-    var cube = document.getElementById("sb"); 
-    cube.addEventListener('click', opensetings);
+    let sb = document.getElementById("sb"); 
+    sb.addEventListener('click', opensetings);
+    let shopbutton = document.getElementById("shopbutton");
+    shopbutton.addEventListener('click',openshop());
     }
 
   function copytext(){
     navigator.clipboard.writeText(document.getElementById("displaycode").textContent);
+  }
+  function addclicker(){
+    if (score >= 15){
+      clicker += 1;
+      score -= 15;
+    }
   }
   getcode();
   Listeners();
