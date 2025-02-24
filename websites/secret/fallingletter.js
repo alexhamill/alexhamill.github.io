6c5ed040d87c830ae336f7db2e636959523a1b20
alexhamill <alexander.hamill@icloud.com>
2025-02-24 08:56:47 -0500

    ADDDED A FUN page (no secrets)

---
+++ websites/secret/fallingletter.js
@@ -0,0 +1,44 @@
+var count = 300;
+var i =0;
+var middle=10;
+var middlecount=0;
+var middlefrac = 4;
+function createFallingLetter() {
+    var letter = document.createElement("p");
+    document.body.appendChild(letter);
+    letter.classList.add("fallingletters");
+    var randomX = Math.floor(Math.random() * window.innerWidth);
+    while(middle>middlecount && randomX>window.innerWidth/2-window.innerWidth/middlefrac && randomX<window.innerWidth/2+window.innerWidth/middlefrac){
+        randomX = Math.floor(Math.random() * window.innerWidth);
+    }
+    if(middlecount>middle && randomX>window.innerWidth/2-window.innerWidth/middlefrac && randomX<window.innerWidth/2+window.innerWidth/middlefrac){
+        middlecount=0;
+    }
+    middlecount++;
+    letter.style.position = "absolute";
+    letter.style.left = randomX + "px";
+    letter.innerText = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+    document.body.appendChild(letter);
+}
+
+function createLettersWithTimeout() {
+    if (i < count) {
+        createFallingLetter();
+        i++;
+        setTimeout(createLettersWithTimeout, 150);
+    }
+}
+createLettersWithTimeout();
+
+createLettersWithTimeout();
+// while (i < count) {
+//     var letter = document.createElement("p");
+//     document.body.appendChild(letter);
+//     letter.classList.add("fallingletters");
+//     var randomX = Math.floor(Math.random() * window.innerWidth);
+//     letter.style.position = "absolute";
+//     letter.style.left = randomX + "px";
+//     letter.innerText = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+//     document.body.appendChild(letter);
+//     i++;
+// }
