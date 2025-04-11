bb0d07d8ffd63c90cca8135db2ad2160d1444efe
alexhamill <alexander.hamill@icloud.com>
2025-04-11 09:22:51 -0400

    listen i almost made the flag

--- websites/newjs/js.js
+++ websites/newjs/js.js
@@ -1,5 +1,60 @@
-function add(x,y){
-    console.log(x+y);
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+function circle(x,y,r){
+    ctx.beginPath();
+    ctx.arc(x,y,r,0,2*Math.PI);
+    ctx.fillStyle = "white";
+    ctx.fill();
 }
 
-add(2,3);
+function snowman(x,y){
+    circle(x,y,10);
+    circle(x,y+20,20);
+    circle(x,y+50,30);
+}
+
+function star(x,y, s){
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x + s, y + 3*s);
+    ctx.lineTo(x - s, y + s);
+    ctx.lineTo(x + s, y + s);
+    ctx.lineTo(x - s, y + 3*s);
+    ctx.closePath();
+    ctx.fillStyle = "white";
+    ctx.fill();
+}
+function flagback(x,y){
+    var i = 0;
+    ctx.fillStyle = "white";
+    ctx.fillRect(x,y,500,260);
+    ctx.fillStyle = "red";
+    while (i < 7){
+    ctx.fillRect(x,y+40*i,500,20);
+    i++;
+    }
+}
+function flagstars(x,y){
+    var i = 0;
+    var u = 0; 
+    var col = 0;
+    ctx.fillStyle = "blue";
+    ctx.fillRect(x,y,200,100);
+    while (i < 9){
+        while (col < 5-i%2){
+            if (i%2 == 0){
+                u = 20;
+            } else {
+                u = 10;
+            }
+            star(x+40*col+10,y+20*i,5);
+            col++;
+        }
+    i++;
+    }
+}
+function flag(x,y){
+    flagback(x,y);
+    flagstars(x,y);
+}
+flag(0,0);
